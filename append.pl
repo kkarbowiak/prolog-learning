473d2d4525a4c5cd6139c2766143f84a27d0761f
kkarbowiak <kkarbowiak@localhost>
2016-08-23 23:30:02 +0200

    Generalise list append rule for all list lengths

--- append.pl
+++ append.pl
@@ -1,4 +1,3 @@
 append_list([], List, List).
-append_list([Head|[]], List, [Head|List]).
-append_list([Head1|[Head2|[]]], List, [Head1, Head2|List]).
+append_list([Head|Tail1], List, [Head|Tail2]) :- append_list(Tail1, List, Tail2).
 
